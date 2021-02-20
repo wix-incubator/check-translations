88f9363bbee97c1da4d1962006ed33d4272214a1
Mantas Miliukas <mantasm@wix.com>
2021-02-20 20:32:40 +0200

    fix: broken reporting

--- index-missing-translations.js
+++ index-missing-translations.js
@@ -9,15 +9,15 @@ function checkMissingTranslations(translationsPath) {
   const files = getFileNames(translationsPath)
     .filter(file => !file.endsWith('_en.json'));
   
-  const arFile = files.find(name => name.endsWith('_ar.json'));
-  if (!arFile) {
+  const arFilePath = path.join(translationsPath, 'messages_ar.json');
+  if (!fs.existsSync(arFilePath)) {
     return {};
   }
 
-  const arJson = require(path.join(translationsPath, file));
+  const arJson = readAsJson(arFilePath);
 
   return files.reduce((result, file) => {
-    const json = require(path.join(translationsPath, file));
+    const json = readAsJson(path.join(translationsPath, file));
     Object.keys(json).forEach(key => {
       if (json[key] === arJson[key]) {
         result[file] = result[file] || {};
@@ -26,4 +26,8 @@ function checkMissingTranslations(translationsPath) {
     });
     return result;
   }, {});
+}
+
+function readAsJson(file) {
+  return JSON.parse(fs.readFileSync(file, { encoding: 'UTF8' }));
 }
