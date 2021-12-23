a28972bcc7c7c83fa3ddbbf708fcbf3169f9c020
gerdasi <gerdasi@wix.com>
2021-12-23 16:22:41 +0200

    don't use dirname

--- search-translations.js
+++ search-translations.js
@@ -5,8 +5,7 @@ const fs = require('fs');
 
 console.log('Running search-translations...');
 
-const directoryPath = path.join(__dirname, process.argv[2]);
-const allFiles = walk(directoryPath);
+const allFiles = walk(process.argv[2]);
 const files = allFiles.filter((f) => !f.includes('messages_'));
 const translationFile = allFiles.filter((f) => f.includes('messages_en'))[0];
 const allKeys = getAllKeys(translationFile);
